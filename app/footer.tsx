c9601c7cc78548faf9fc4da40854be46588111e1
telliex.chiu <telliexyuzo@gmail.com>
2025-03-13 02:00:41 -0700

    fix(): Fix footer issue.

--- app/footer.tsx
+++ app/footer.tsx
@@ -73,6 +73,7 @@ export function Footer() {
         <a href="https://github.com/ibelick/nim" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
             <span>© 2024 Activo</span>
+            <span>Built with Motion-Primitives.</span>
           </TextLoop>
         </a>
         <div className="text-xs text-zinc-400">
